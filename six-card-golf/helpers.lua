71760cffa8562cba48404e5076599c1554fcbd13
Christophe Gagnier <c.gagnier11@gmail.com>
2020-11-18 08:28:40 -0500

    Added function to find best score

--- six-card-golf/helpers.lua
+++ six-card-golf/helpers.lua
@@ -38,4 +38,31 @@ function helpers.hasValue (tab, val)
     return false
 end
 
+-- Will return the index of the card that should be replaced, OR -1 if nothing should be replaced
+function helpers.defineBestAction(board, pile) 
+    bestIndex = -1
+    bestScore = helpers.getScore(board)
+
+    print("Best Score ".. bestScore)
+
+    for index,value in ipairs(board) do
+        tempBoard = table.clone(board)
+        table.remove( tempBoard, index)
+        table.insert( tempBoard, pile)
+        
+        tempScore = helpers.getScore(tempBoard)
+        print("Current temp Score ".. tempScore.. " with index "..index)
+        if (tempScore < bestScore) then
+            bestScore = tempScore
+            bestIndex = index
+        end
+    end
+
+    return bestIndex
+end
+
+function table.clone(org)
+    return {table.unpack(org)}
+end
+
 return helpers
--- six-card-golf/test/tests.lua
+++ six-card-golf/test/tests.lua
@@ -1,22 +1,27 @@
 helpers = require("helpers")
 
 hand1 = {
-    { suit = 'joker', rank = 1, flipped = false},
-    { suit = 'heart', rank = 1, flipped = false},
-    { suit = 'spade', rank = 3, flipped = false},
-    { suit = 'spade', rank = 13, flipped = false},
-    { suit = 'club', rank = 1, flipped = false},
-    { suit = 'diamond', rank = 4, flipped = false},
+    { suit = 'joker', rank = 0, flipped = true},
+    { suit = 'heart', rank = 1, flipped = true},
+    { suit = 'spade', rank = 3, flipped = true},
+    { suit = 'spade', rank = 13, flipped = true},
+    { suit = 'club', rank = 1, flipped = true},
+    { suit = 'diamond', rank = 4, flipped = true},
 }
 
 hand2 = {
-    { suit = 'spade', rank = 4, flipped = false},
-    { suit = 'heart', rank = 5, flipped = false},
-    { suit = 'spade', rank = 6, flipped = false},
-    { suit = 'spade', rank = 7, flipped = false},
-    { suit = 'club', rank = 1, flipped = false},
-    { suit = 'diamond', rank = 4, flipped = false},
+    { suit = 'spade', rank = 4, flipped = true},
+    { suit = 'heart', rank = 5, flipped = true},
+    { suit = 'spade', rank = 6, flipped = true},
+    { suit = 'spade', rank = 7, flipped = true},
+    { suit = 'club', rank = 1, flipped = true},
+    { suit = 'diamond', rank = 4, flipped = true},
 }
+
+pile1 = { suit = 'joker', rank = 0, flipped = true}
+pile2 = { suit = 'club', rank = 6, flipped = true},
 
 assert(helpers.getScore(hand1) == 2, "Test on Jokers and Kings failed")
 assert(helpers.getScore(hand2) == 19, "Test on regular card failed")
+assert(helpers.defineBestAction(hand1,pile1) == 6, "Test on best action with joker failed")
+assert(helpers.defineBestAction(hand2,pile2) == 4, "Test on best action with duplicate failed")
